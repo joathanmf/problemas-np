07fc27a651c1e7e8b721b48e9817396d80f4991a
Joathan Mareto <joathanmareto@gmail.com>
2022-07-18 14:29:48 -0300

    criação do main e import itertools

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
-arquivo = open("grafo")
-grafo = {}
+from itertools import combinations as comb
 
-linhas = arquivo.readlines()
 
-for linha in linhas:
-    linha = linha.split(" ")
-    grafo.update({linha[0]: []})
+def inicializa_grafo(grafo):
+    arquivo = open("grafo.txt", "r", encoding="utf-8")
 
-for linha in linhas:
-    linha = linha.split(" ")
-    valor = linha[1].split("\n")
-    valor_grafo = grafo.get(linha[0])
-    grafo.update({linha[0]: valor_grafo + [valor[0]]})
+    linhas = arquivo.readlines()
 
-# print(valor_grafo)
+    for linha in linhas:
+        linha = linha.split(" ")
+        grafo.update({linha[0]: []})
 
-# print(grafo.items())
+    for linha in linhas:
+        linha = linha.split(" ")
+        valor = linha[1].split("\n")
+        valor_grafo = grafo.get(linha[0])
+        grafo.update({linha[0]: valor_grafo + [valor[0]]})
+
+    arquivo.close()
+
+    return grafo
+
+
+def combinatoria(grafo):
+    comb(grafo.keys())
+
+
+def verifica_solucao(grafo, solucao):
+    pass
+
+
+def main():
+    grafo = {}
+    grafo = inicializa_grafo(grafo)
+    # print(grafo.keys())
+
+    # solucao = [1, 2]
+
+    # verifica_solucao(grafo, solucao)
+
+
+main()
